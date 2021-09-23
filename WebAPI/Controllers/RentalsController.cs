b02d14e92b99d4977b22646cd885fa81c4a6a78c
zumragirgin <zumragirgin@ogr.sakarya.edu.tr>
2021-09-23 17:28:36 +0300

    11. Gün Sonu

---
+++ WebAPI/Controllers/RentalsController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        IRentalsService _rentalsService;
+        public RentalsController(IRentalsService rentalsService)
+        {
+            _rentalsService = rentalsService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _rentalsService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Rental rental)
+        {
+            var result = _rentalsService.Add(rental);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
